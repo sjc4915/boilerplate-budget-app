9c01d2cbc0116a5083feaeef7fe4c62a2d37b34d
Firstname Lastname <email_address>
2021-05-12 12:41:19 -0500

    from PyCharm

--- budget.py
+++ budget.py
@@ -1,5 +1,6 @@
 class Category:
-
+#Adding some comments to confirm that pushes from PyCharm to GitHub will also show up in Replit.
+#Make an object diagram from the README.md project description
 
 
 
