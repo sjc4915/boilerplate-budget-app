695cf2c12d0b11f829bbd0eb649e07c62047d03d
Stephen Caldwell <>
2021-05-12 17:46:32 +0000

    Added comment to demonstrate to self a round trip to PyCharm

--- budget.py
+++ budget.py
@@ -1,7 +1,7 @@
 class Category:
 #Adding some comments to confirm that pushes from PyCharm to GitHub will also show up in Replit.
 #Make an object diagram from the README.md project description
-
+#Adding additional info to push back to PyCharm to confirm round trip.
 
 
 
